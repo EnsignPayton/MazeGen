a1af45521d028b3e581390d8b7b6736e18881a4d
Logan Payton <Logan.Payton@javs.com>
2017-04-07 20:23:21 -0400

    WIP

--- maze.c
+++ maze.c
@@ -6,4 +6,20 @@
 
 #include "maze.h"
 
+// Generate a maze using randomized depth-first
+// * Start with a random cell
+// * Select random unvisited neighbor
+// * Link cells, mark new as visited
+// * Add to stack and repeat with new node
+// * When dead end, backtrack up stack
 
+// Contain ... pick this back up
+typedef struct cell {
+    int nCell;
+    // TODO: Switch to flags in single byte if this takes up too much space
+    bool visited;
+    bool up;
+    bool down;
+    bool left;
+    bool right;
+} Cell;
